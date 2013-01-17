54a49b551270e873f16466dc56893f6f60751101
First1 <First1@ROCFIRSTLABXP11.blamericas.bausch.com>
2013-01-17 17:23:10 -0500

    Wrote comments for timer code.

--- FRC2013/src/edu/wpi/first/wpilibj/templates/FRCTimer.java
+++ FRC2013/src/edu/wpi/first/wpilibj/templates/FRCTimer.java
@@ -17,18 +17,32 @@ public class FRCTimer {
         clock= new Timer();
     }
     
+    /**
+     * Start the timer
+     */
     public static void start(){
         clock.start();
     }
     
+    
+    /**
+     * Stop the timer
+     */
     public static void stop(){
         clock.stop();
     }
     
+    /**
+     * Resets the timer
+     */
     public static void reset(){
         clock.reset();
     }
     
+    /**
+     * Gets the current time on the timer in seconds
+     * @return time in seconds
+     */
     public double getSeconds(){
         return clock.get() / 1000000;
     }
