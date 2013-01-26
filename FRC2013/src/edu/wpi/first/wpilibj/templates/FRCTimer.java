03fcc0b98844d379be666f967d2998b97b0d62bd
first1 <first1@BAUSCH-621CA93D.blamericas.bausch.com>
2013-01-26 14:02:26 -0500

    added timery stuff - lauren

--- FRC2013/src/edu/wpi/first/wpilibj/templates/FRCTimer.java
+++ FRC2013/src/edu/wpi/first/wpilibj/templates/FRCTimer.java
@@ -13,15 +13,35 @@ import edu.wpi.first.wpilibj.Timer;
 public class FRCTimer {
     
     public static Timer clock;
+    public static boolean bHasStarted;
     public static void initTimer(){
+        bHasStarted = false;
         clock= new Timer();
+        
     }
-    
+    public static boolean DelayDone(double length){
+        if (bHasStarted == true){
+           if (getSeconds() >= length) { 
+               stop();
+               return true;
+               
+           }
+           else {
+              return false;
+           }
+        } else {
+           start();
+        }
+        return false;
+        
+    }
+
     /**
      * Start the timer
      */
     public static void start(){
         clock.start();
+        bHasStarted = true ;
     }
     
     
@@ -30,6 +50,7 @@ public class FRCTimer {
      */
     public static void stop(){
         clock.stop();
+        bHasStarted = false;
     }
     
     /**
@@ -43,7 +64,7 @@ public class FRCTimer {
      * Gets the current time on the timer in seconds
      * @return time in seconds
      */
-    public double getSeconds(){
+    public static double getSeconds(){
         return clock.get() / 1000000;
     }
     
