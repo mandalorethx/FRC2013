cf72406d57bd08c2f3afc9ed24fd78b9811ac57d
first1 <first1@ROCFIRST1XP04.blamericas.bausch.com>
2013-01-26 11:39:20 -0500

    Gathered and set output states in EnhancedIOBoard.

--- FRC2013/src/edu/wpi/first/wpilibj/templates/EnhancedIOBoard.java
+++ FRC2013/src/edu/wpi/first/wpilibj/templates/EnhancedIOBoard.java
@@ -4,7 +4,9 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStationEnhancedIO;
+import edu.wpi.first.wpilibj.DriverStationEnhancedIO.EnhancedIOException;
 
 /**
  *
@@ -14,7 +16,25 @@ public class EnhancedIOBoard {
     
     public static DriverStationEnhancedIO EIO;
     public static void initDriverStationEnhancedIO(){
-       //EIO = DriverStation
+       EIO = DriverStation.getInstance().getEnhancedIO();   
     }
     
+    public static void setOutputState(int channel, boolean value){
+        try {
+            EIO.setDigitalOutput(channel, value);
+        } catch (EnhancedIOException ex) {
+            ex.printStackTrace();
+        }      
+    }
+    
+    public static boolean getOutputState(int channel){
+        try {
+            return EIO.getFixedDigitalOutput(channel);
+        } catch (EnhancedIOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            return false;
+        }
+    }
 }
